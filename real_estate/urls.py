1f4cd98ddcbf97dcb94bc7c8ece1e64baf81b462
Vladislav Mykhailovskyi <vladklp22@gmail.com>
2023-05-14 12:57:14 +0300

    setup docker with compose

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,14 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/v1/auth/', include('djoser.urls')),
-                  path('api/v1/auth/', include('djoser.urls.jwt')),
-                  path('api/v1/profile/', include('apps.profiles.urls')),
-                  path('api/v1/properties/', include('apps.properties.urls')),
-                  path('api/v1/ratings/', include('apps.ratings.urls')),
-                  path('api/v1/enquiries/', include('apps.enquiries.urls'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path('api/v1/profile/', include('apps.profiles.urls')),
+    path('api/v1/properties/', include('apps.properties.urls')),
+    path('api/v1/ratings/', include('apps.ratings.urls')),
+    path('api/v1/enquiries/', include('apps.enquiries.urls'))
+]
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = " Real Estate Admin Portal"
